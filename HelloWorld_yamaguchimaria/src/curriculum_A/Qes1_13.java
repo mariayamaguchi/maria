e989599b1a0326598bec0924da3372827f4fe44c
maria <maria@192.168.1.4>
2025-09-17 21:45:45 +0900

    課題再提出２

--- HelloWorld_yamaguchimaria/src/curriculum_A/Qes1_13.java
+++ HelloWorld_yamaguchimaria/src/curriculum_A/Qes1_13.java
@@ -24,7 +24,7 @@ public class Qes1_13 {
 		tannsei = 0.0f;
 		bai = 0.0;
 		mozi = '\u0000';
-		moziretu = "null";
+		moziretu = null;
 		buriann = false;
 
 		//3
@@ -39,21 +39,21 @@ public class Qes1_13 {
 		buriann = true;
 		
 		//4
-		System.out.println(baito+syoto+intt+nagai);
+		System.out.println(baito + syoto + intt + nagai);
 		double sum = (baito + bai);
 		int res = (int) sum;
 		System.out.println(res);
-		System.out.println(mozi+" "+moziretu+" "+buriann);
-		System.out.println((int) (baito+syoto+intt+nagai+sum));
-		System.out.println(baito*syoto*intt*nagai);
-		System.out.println(bai/syoto);
-		System.out.println(baito-syoto);
+		System.out.println(mozi + " " + moziretu + " " + buriann);
+		System.out.println((int) (baito + syoto + intt+nagai + sum));
+		System.out.println(baito * syoto * intt * nagai);
+		System.out.println(bai / syoto);
+		System.out.println(baito - syoto);
 		
 		//5
 		String num="20";
 		int num2 = Integer.parseInt(num);
 		int num1=23;
-		System.out.println("ハローJAVA"+(num2+num1));
+		System.out.println("ハローJAVA"+(num2 + num1));
 		
 		//6
 		String name = "山田太郎";
@@ -61,15 +61,15 @@ public class Qes1_13 {
 		double height = 170.5;
 		double weight = 62.2;
 		String food = "寿司";
-		System.out.println("初めまして"+name+"です");
-		System.out.println("年齢は"+age+"歳です");
-		System.out.println("身長は"+height+"cmです");
-		System.out.println("体重は"+weight+"kgです");
-		System.out.println("好きな食べ物は"+food+"です");
+		System.out.println("初めまして" + name + "です");
+		System.out.println("年齢は" + age + "歳です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("体重は" + weight + "kgです");
+		System.out.println("好きな食べ物は" + food + "です");
 		
 		//7
-		double heightBmi = height/100;
-		System.out.println("BMIは"+(double)Math.round(weight/(heightBmi*heightBmi))+"です");
+		double heightBmi = height / 100;
+		System.out.println("BMIは" + (double)Math.round(weight / (heightBmi * heightBmi)) + "です");
 		
 		//8
 		name = "鈴木一郎";
@@ -77,19 +77,19 @@ public class Qes1_13 {
 		height = 168.5;
 		weight = 64.2;
 		food = "オムライス";
-		System.out.println("初めまして"+name+"です");
-		System.out.println("年齢は"+age+"歳です");
-		System.out.println("身長は"+height+"cmです");
-		System.out.println("体重は"+weight+"kgです");
-		System.out.println("好きな食べ物は"+food+"です");
+		System.out.println("初めまして" + name + "です");
+		System.out.println("年齢は" + age + "歳です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("体重は" + weight + "kgです");
+		System.out.println("好きな食べ物は" + food + "です");
 		
-		heightBmi = height/100;
-		bai = (weight/(heightBmi*heightBmi));
+		heightBmi = height / 100;
+		bai = (weight / (heightBmi * heightBmi));
 		double parm = bai;
 		double expo=1;
 	    double base=10;
 	    double result=Math.pow(base,expo);
-	    System.out.println("BMIは"+Math.round(parm*result)/result+"です");
+	    System.out.println("BMIは" + Math.round(parm * result) / result + "です");
 		
 		//9
 		name = "鈴木一郎";
@@ -97,21 +97,21 @@ public class Qes1_13 {
 		height = 168.5;
 		weight = 64.2;
 		age = age+age;
-		height = height+height;
-		weight = weight+weight;
-		System.out.println("初めまして"+name+"です");
-		System.out.println("年齢は"+age+"歳です");
-		System.out.println("身長は"+height+"cmです");
-		System.out.println("体重は"+weight+"kgです");
-		System.out.println("好きな食べ物は"+food+"です");
+		height = height + height;
+		weight = weight + weight;
+		System.out.println("初めまして" + name + "です");
+		System.out.println("年齢は" + age + "歳です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("体重は" + weight + "kgです");
+		System.out.println("好きな食べ物は" + food + "です");
 		
-		heightBmi = height/100;
-		bai = (weight/(heightBmi*heightBmi));
+		heightBmi = height / 100;
+		bai = (weight / (heightBmi * heightBmi));
 		double parma = bai;
 		double expoa=1;
 	    double basea=10;
 	    double resulta=Math.pow(basea,expoa);
-	    System.out.println("BMIは"+Math.round(parma*result)/resulta+"です");
+	    System.out.println("BMIは" + Math.round(parma * result) / resulta + "です");
 		
 		//10
 	    boolean isOverage;
@@ -127,7 +127,7 @@ public class Qes1_13 {
 		String age2 = String.valueOf(age);
 		String height2 = String.valueOf(height);
 		String weight2 = String.valueOf(weight);
-		System.out.println(age2+height2+weight2);
+		System.out.println(age2 + height2 + weight2);
 		 
 		//12
 	    double db = Double.parseDouble(height2);
